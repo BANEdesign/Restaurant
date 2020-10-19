ff9dc76df3009abcaa1673fc80deeada5d9fd5a3
Bryonna Baines <bryonna.baines@nike.com>
2020-10-19 14:22:49 -0700

    added favorite button

--- app/src/main/java/com/breebanes/restaurant/ui/RestaurantListAdapter.kt
+++ app/src/main/java/com/breebanes/restaurant/ui/RestaurantListAdapter.kt
@@ -13,9 +13,11 @@ import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.restaurant_list_item.view.*
 import java.util.*
 
-class RestaurantListAdapter(private val shops: List<Restaurant>) : RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder>() {
+class RestaurantListAdapter(private val shops: List<Restaurant>,
+                            private val favShops: List<String>? = null) : RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder>() {
 
     var onItemClick: ((Restaurant) -> Unit)? = null
+    var onFavClick: ((Restaurant) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestaurantViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.restaurant_list_item, parent, false)
@@ -33,12 +35,17 @@ class RestaurantListAdapter(private val shops: List<Restaurant>) : RecyclerView.
                 itemView.setOnClickListener {
                     onItemClick?.invoke(shops[adapterPosition])
                 }
+                itemView.favoriteButton.setOnClickListener {
+                    onFavClick?.invoke(shops[adapterPosition])
+                }
             }
         val imageView: ImageView = itemView.restaurantImageView
         val name: TextView = itemView.itemName
         val description: TextView = itemView.itemDescription
         val deliveryFee: TextView = itemView.deliveryFee
         val status: TextView = itemView.status
+        val favoriteButton: TextView = itemView.favoriteButton
+
         fun bind(shop: Restaurant) {
             Glide
                 .with(itemView)
@@ -51,7 +58,13 @@ class RestaurantListAdapter(private val shops: List<Restaurant>) : RecyclerView.
             if (shop.status.toLowerCase(Locale.US).contains("closed"))
                 status.setTextColor(ContextCompat.getColor(itemView.context, R.color.colorPrimary))
             status.text = shop.status
-
+            favoriteButton.apply {
+                if (favShops != null
+                    && favShops.contains(shops[adapterPosition].id)) {
+                    text = "Unfavorite"
+                    setTextColor(ContextCompat.getColor(itemView.context, R.color.design_default_color_primary_dark))
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/breebanes/restaurant/ui/RestaurantListFragment.kt
+++ app/src/main/java/com/breebanes/restaurant/ui/RestaurantListFragment.kt
@@ -1,5 +1,7 @@
 package com.breebanes.restaurant.ui
 
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -21,6 +23,7 @@ class RestaurantListFragment : Fragment() {
     lateinit var viewModel: RestaurantViewModel
     lateinit var listAdapter: RestaurantListAdapter
     lateinit var navController: NavController
+    var shopList: MutableList<String> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,16 +42,28 @@ class RestaurantListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val prefs = activity?.getPreferences(MODE_PRIVATE)
+        prefs?.let {
+            //var set = listOf<String>()
+            val set = it.getStringSet(REST_ID, setOf(""))
+            if (set != null && set.size > 1) {
+                shopList = set.toMutableList()
+            }
+        }
+
         viewModel.restaurantList.observe(viewLifecycleOwner, Observer {
             progressBar.visibility = View.GONE
             restaurantRecyclerView.apply {
                 layoutManager = LinearLayoutManager(view.context)
-                listAdapter = RestaurantListAdapter(it)
+                listAdapter = RestaurantListAdapter(it, shopList)
                 adapter = listAdapter
                 addItemDecoration(DividerItemDecoration(view.context, LinearLayout.VERTICAL))
                 listAdapter.onItemClick = { restaurant ->
                     navController.navigate(R.id.action_fragmentSearch_to_restaurantDetailFragment,
                         bundleOf(SHOP_ID to restaurant.id))
+                }
+                listAdapter.onFavClick = { shop ->
+                    shopList.add(shop.id)
                 }
             }
         })
@@ -57,9 +72,25 @@ class RestaurantListFragment : Fragment() {
         progressBar.visibility = View.VISIBLE
     }
 
+    override fun onStop() {
+        super.onStop()
+        saveToPrefrences()
+    }
+
+    private fun saveToPrefrences() {
+        activity?.let {
+            //save rest id
+            val prefs: SharedPreferences = it.getPreferences(MODE_PRIVATE)
+            val prefsEditor = prefs.edit()
+            prefsEditor.putStringSet(REST_ID, shopList.toSet())
+            prefsEditor.commit()
+        }
+    }
+
     companion object {
         const val LAT = "37.422740"
         const val LONG = "-122.139956"
+        const val REST_ID = "id"
         val TAG = RestaurantListFragment::class.java.simpleName
     }
 }
